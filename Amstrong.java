2769d15fe2f56d0058c0f4c1bbfaf6c00ffc08a2
Surya-Pakkirisamy <suriya15062001@gmail.com>
2023-12-19 22:51:39 +0530

    i have changed the code with using inbuild methods

--- Amstrong.java
+++ Amstrong.java
@@ -9,14 +9,8 @@ public class Amstrong {
 		System.out.print("Enter the Number :");
 		Scanner scanner=new Scanner(System.in);
 		int number = scanner.nextInt();
-		int length=length(number);
-		if(length == 1)
-		{
-			System.out.println(number+" is a amstrong");
-		}
-		else
-		{
-			if(isAmstrong(number,length) == number)
+		int length=String.valueOf(number).length();
+			if(length == 1 || isArmstrong(number,length))
 			{
 				System.out.println(number+" is a amstrong");
 			}
@@ -24,42 +18,20 @@ public class Amstrong {
 			{
 				System.out.println(number+" is not a amstrong");
 			}
-		}
 		scanner.close();
 		
 	}
-	public static int isAmstrong(int number, int length) 
+	public static boolean isArmstrong(int number,int length) 
 	{
 		int sum=0;
+		int originalNum=number;
 		while(number > 0)
 		{
 			int rem=number%10;
-			sum+=power(rem,length);
+			sum+=(int)Math.pow(rem, length);
 			number/=10;
 		}
-		return sum;
-	}
-	
-	
-
-	public static int power(int rem, int length) 
-	{
-		int power=1;
-		for (int i=0;i<length;i++)
-		{
-			power*=rem;
-		}
-		return power;
-	}
-	public static int length(int number) 
-	{
-		int length=0;
-		while(number > 0)
-		{
-			length++;
-			number/=10;
-		}
-		return length;
+		return originalNum == sum;
 	}
 
 }
